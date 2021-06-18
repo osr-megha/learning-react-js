ba1898912f414acdeccd547e62c0fb66bafb803c
Saurabh Sharma <osr.megha>
2021-06-18 15:20:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from "react";
+import ComA from "./ComA";
+
+const FirstName = createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <FirstName.Provider>
+        <ComA />
+      </FirstName.Provider>
+    </React.Fragment>
   );
 }
 
---
+++ src/ComC.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComC() {
+  return (
+    <React.Fragment>
+      <h1> I am Com C </h1>
+    </React.Fragment>
+  );
+}
+
+export default ComC;
